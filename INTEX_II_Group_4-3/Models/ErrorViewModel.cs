3ed8d8af6e36adc1060b6c0da357a62d3ffcf732
c-woodman <cwoodma1@byu.edu>
2024-04-09 13:31:19 -0600

    Add project files.

---
+++ INTEX_II_Group_4-3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INTEX_II_Group_4_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

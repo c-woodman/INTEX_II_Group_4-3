114d63a6599ffa34c33cc33a77ac12f388b59951
c-woodman <cwoodma1@byu.edu>
2024-04-09 14:20:08 -0600

    finalizing updates

---
+++ INTEX_II_Group_4-3/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INTEX_II_Group_4_3.Models;
+
+public partial class Product
+{
+    public double? ProductId { get; set; }
+
+    public string? Name { get; set; }
+
+    public double? Year { get; set; }
+
+    public double? NumParts { get; set; }
+
+    public double? Price { get; set; }
+
+    public string? ImgLink { get; set; }
+
+    public string? PrimaryColor { get; set; }
+
+    public string? SecondaryColor { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Category { get; set; }
+}

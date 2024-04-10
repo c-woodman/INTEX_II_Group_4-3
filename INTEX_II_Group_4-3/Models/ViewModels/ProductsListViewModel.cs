aefaf6951315469cf3d972c769b4bf67be196239
suyeol <suyeol@SUYEOL5544>
2024-04-10 12:24:38 -0600

    Pagination

---
+++ INTEX_II_Group_4-3/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace INTEX_II_Group_4_3.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IQueryable<Product> Products { get; set;}
+
+        public PaginationInfo PaginationInfo { get; set;} = new PaginationInfo();
+    }
+}

6f0c5270e03e835ac7a21dd95db20b4e625bc299
suyeol <suyeol@SUYEOL5544>
2024-04-10 13:36:14 -0600

    Category Filter

--- INTEX_II_Group_4-3/Models/ViewModels/ProductsListViewModel.cs
+++ INTEX_II_Group_4-3/Models/ViewModels/ProductsListViewModel.cs
@@ -5,5 +5,7 @@
         public IQueryable<Product> Products { get; set;}
 
         public PaginationInfo PaginationInfo { get; set;} = new PaginationInfo();
+
+        public string? CurrentProductCategory { get; set;}
     }
 }

6ab3bdc367d073abc43d304ba9c4b2644e6a1eae
maleahy <maleahyee@gmail.com>
2024-04-10 10:44:16 -0600

    checkout and css

--- INTEX_II_Group_4-3/Controllers/HomeController.cs
+++ INTEX_II_Group_4-3/Controllers/HomeController.cs
@@ -30,10 +30,6 @@ namespace INTEX_II_Group_4_3.Controllers
             var products = await _context.Products.ToListAsync();
             return View(products);
         }
-        public IActionResult Checkout()
-        {
-            return View();
-        }
 
         public IActionResult Shop()
         {
@@ -70,6 +66,27 @@ namespace INTEX_II_Group_4_3.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // Render the Checkout View
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            //autofill name from Customer table
+            ViewBag.first_name = _context.Customers.ToList();
+            ViewBag.last_name = _context.Customers.ToList();
+            return View("Checkout");
+        }
+
+        // Post Checkout to database
+        [HttpPost]
+        public IActionResult Order(Order response)
+        {
+            _context.Orders.Add(response);
+            _context.SaveChanges();
+
+            return View("Confirmation", response);
+
         }
     }
 }

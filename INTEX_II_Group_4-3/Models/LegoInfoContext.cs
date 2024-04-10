1742e73b99f42123a58d216d6d8605e5f5fdb006
zeinmusarsaa <zeinmusarsaa@gmail.com>
2024-04-09 19:54:59 -0600

    i hate this with the bottom of my soul!

--- INTEX_II_Group_4-3/Models/LegoInfoContext.cs
+++ INTEX_II_Group_4-3/Models/LegoInfoContext.cs
@@ -36,7 +36,6 @@ public partial class LegoInfoContext : DbContext
     public virtual DbSet<Product> Products { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=tcp:myfreesqldbserverintex3.database.windows.net,1433;Initial Catalog=LegoInfo;Persist Security Info=False;User ID=IntexGroup4-3;Password=Intex1212!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)

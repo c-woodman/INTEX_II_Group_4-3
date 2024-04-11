7616c7b5e81565653cf179d48c540c95c5179765
zeinmusarsaa <zeinmusarsaa@gmail.com>
2024-04-11 16:49:15 -0600

    this is fine i just removed the admin email

--- INTEX_II_Group_4-3/Program.cs
+++ INTEX_II_Group_4-3/Program.cs
@@ -105,32 +105,6 @@ public class Program
         {
             var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
-            //string email = "aurorabrickwell@masterbuilder.com";
-            //string password = "123Intex!";
-
-            //if (await userManager.FindByEmailAsync(email) == null)
-            //{
-            //    var user = new IdentityUser();
-            //    user.UserName = email;
-            //    user.Email = email;
-
-
-            //    await userManager.CreateAsync(user, password);
-
-            //    await userManager.AddToRoleAsync(user, "Admin");
-            //}
-
-            //if (await userManager.FindByEmailAsync(email) == null)
-            //{
-            //    var user = new IdentityUser();
-            //    user.UserName = email;
-            //    user.Email = email;
-
-            //    await userManager.CreateAsync(user, password);
-
-            //    await userManager.AddToRoleAsync(user, "Customer"); // Assign "Customer" role
-            //}
-
         }
 
         app.Run();

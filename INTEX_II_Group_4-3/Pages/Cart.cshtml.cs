964d03568b5642f616174697929b477f96c1ef64
suyeol <suyeol@SUYEOL5544>
2024-04-10 18:37:34 -0600

    CheckPoint

--- INTEX_II_Group_4-3/Pages/Cart.cshtml.cs
+++ INTEX_II_Group_4-3/Pages/Cart.cshtml.cs
@@ -1,3 +1,4 @@
+using INTEX_II_Group_4_3.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,23 @@ namespace INTEX_II_Group_4_3.Pages
 {
     public class CartModel : PageModel
     {
+        private ILegoRepository _repo;
+        public CartModel(ILegoRepository temp) 
+        {
+            _repo = temp;
+        }
+        public Cart? Cart { get; set; }
         public void OnGet()
         {
+        }
+        public void OnPost(int productId)
+        {
+            Product prod = _repo.Products
+                .FirstOrDefault(x =>  x.ProductId == productId);
+
+            Cart = new Cart();
+
+            Cart.AddItem(prod, 1);
         }
     }
 }

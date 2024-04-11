a77d6dcc640894cefae9a60c47b880f6e2d97a6c
suyeol <suyeol@SUYEOL5544>
2024-04-10 21:43:27 -0600

    Remove feature to Cart

--- INTEX_II_Group_4-3/Program.cs
+++ INTEX_II_Group_4-3/Program.cs
@@ -18,6 +18,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
 
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)

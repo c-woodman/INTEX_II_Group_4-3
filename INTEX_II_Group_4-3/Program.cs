032f188f887afee26bc4932cae789240b479057e
zeinmusarsaa <zeinmusarsaa@gmail.com>
2024-04-12 10:29:16 -0600

    CSP

--- INTEX_II_Group_4-3/Program.cs
+++ INTEX_II_Group_4-3/Program.cs
@@ -86,27 +86,27 @@ public class Program
 
         app.UseAuthorization();
 
-        //app.Use(async (context, next) =>
-        //{
-        //    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-        //    context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
-        //    context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+        app.Use(async (context, next) =>
+        {
+            context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+            context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
+            context.Response.Headers.Add("Referrer-Policy", "no-referrer");
 
-        //    // Define your Content-Security-Policy
-        //    string csp = "default-src 'self'; " +
-        //                 "script-src 'self' 'unsafe-inline'; " +
-        //                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " + // For Google Fonts
-        //                 "img-src 'self' data: https://m.media-amazon.com https://www.lego.com https://images.brickset.com https://www.brickeconomy.com; " + // Domains for images
-        //                 "font-src 'self' https://fonts.gstatic.com;"; // For Google Fonts
+            // Define your Content-Security-Policy
+            string csp = "default-src 'self'; " +
+                         "script-src 'self' 'unsafe-inline'; " +
+                         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " + // For Google Fonts
+                         "img-src 'self' data: https://m.media-amazon.com https://www.lego.com https://images.brickset.com https://www.brickeconomy.com; " + // Domains for images
+                         "font-src 'self' https://fonts.gstatic.com;"; // For Google Fonts
 
-        //    // Add Content-Security-Policy without overwriting existing headers
-        //    if (!context.Response.Headers.ContainsKey("Content-Security-Policy"))
-        //    {
-        //        context.Response.Headers.Add("Content-Security-Policy", csp);
-        //    }
+            // Add Content-Security-Policy without overwriting existing headers
+            if (!context.Response.Headers.ContainsKey("Content-Security-Policy"))
+            {
+                context.Response.Headers.Add("Content-Security-Policy", csp);
+            }
 
-        //    await next();
-        //});
+            await next();
+        });
 
 
         app.MapControllerRoute(
